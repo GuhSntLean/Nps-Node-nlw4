b283c6d3552696903096ad74a0e858bd02f0daa2
GuhSntLean <guleandro.66@gmail.com>
2021-02-22 21:53:40 -0300

    :heavy_plus_sign: Add verify server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,17 @@ import express from 'express';
 const app = express() 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
+app.get('/users', (request, response) => {
+  return response.json({message: "Hello man"});
+});
+
+app.post('/', (request, response) => {
+  return response.json({message: "Mensagem salva"});
+});
+
+app.listen(port, '0.0.0.0', (error) => {
+  if(error){
+    return console.log(`Error try access server: ${error}`)
+  }
   return console.log(`Server is running in port: ${port}`);
 });
